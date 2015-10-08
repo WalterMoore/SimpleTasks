fd400b0bd3726aa17da2c350f181074ef3ec9c7b
bamaswampmonster <bamaswampmonster@bamaswampmonsters-MacBook-Pro.local>
2015-10-08 14:54:44 -0500

    progress on DOM

--- dom.html/dom.js
+++ dom.html/dom.js
@@ -1,4 +1,10 @@
 //1. Make a button that displays an alert when clicked. Use DOM, jQuery, and Angular.
+	function myFunction(){
+		alert("OW! Watch it!");
+	};
+			
+	var btn = document.getElementById('textAlert');
+	btn.onclick=myFunction;
 			/*function createButton() {
 				var myButton = document.createElement('button');
 				var myButtonText = document.createTextNode('Click Me');	
@@ -25,7 +31,12 @@
 //3. Create a div in HTML then add JavaScript to make it change colors whenever your mouse hovers over the div.
  //The div should return to it's original color when the mouse exits the div. Use DOM, jQuery, and Angular.
  
- 
+ 	function myChameleon(e){
+		e.target.style.backgroundColor="red";
+	};
+			
+	var box = document.getElementById('chameleon');
+	box.onmouseover=myChameleon;
  
 //4. Put some text in a paragraph. Make it where when you click on the paragraph text, the color of the text switches to red.
  //Once complete, try making it switch to a random color each click. Use DOM, jQuery, and Angular.
