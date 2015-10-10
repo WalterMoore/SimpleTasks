83cdb98743d3daa39b6d9bda5ce141034f05d10f
bamaswampmonster <bamaswampmonster@bamaswastersmbp.attlocal.net>
2015-10-09 23:11:15 -0500

    halfway through jQuery

--- jQuery.html/j.js
+++ jQuery.html/j.js
@@ -1,13 +1,29 @@
+$(document).ready(function(){
 //1. Make a button that displays an alert when clicked. Use DOM, jQuery, and Angular.
-
+    $("#textAlert").click(function(){
+        alert("Hey! Stop that.");
+    });
 
 //2. Make a button and text box. When the button is clicked,
 // display an alert with the message that is typed in the text box. Use DOM, jQuery, and Angular.
- 
+ 	
+	
+	//$("#textbox").val("bla");
+	 
+	 $("#clickText").click(function(){
+		 var bla = $("#textbox").val();
+		 alert(bla);
+	});
  
 //3. Create a div in HTML then add JavaScript to make it change colors whenever your mouse hovers over the div.
  //The div should return to it's original color when the mouse exits the div. Use DOM, jQuery, and Angular.
- 
+	$("#chameleon").hover(function() {
+		console.log("I happened");
+		$(this).addClass("hover");
+	}, 
+		function() { 
+			$(this).removeClass("hover");
+	});
  
  
 //4. Put some text in a paragraph. Make it where when you click on the paragraph text, the color of the text switches to red.
@@ -21,3 +37,4 @@
 
 
 //6.Create a <ul> in HTML. From JavaScript add an <li> for items in an array that you create. Use DOM, jQuery, and Angular.
+});
